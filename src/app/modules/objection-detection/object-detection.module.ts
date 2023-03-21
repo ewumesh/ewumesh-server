b0bfbd86e30d564d92b258fc769916d81d13e39f
umeshthapa12 <umeshthapa112233@gmail.com>
2023-03-21 10:48:53 +0545

    fix: added language translator in Object detection module

--- src/app/modules/objection-detection/object-detection.module.ts
+++ src/app/modules/objection-detection/object-detection.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObjectDetectionComponent } from './object-detection.component';
 import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient){
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  }
 
 @NgModule({
     declarations: [ObjectDetectionComponent],
     imports: [ 
         CommonModule,
+        TranslateModule.forChild({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+          }),
+          HttpClientModule,
         RouterModule.forChild([{path:'', component: ObjectDetectionComponent}])
      ],
     exports: [],
